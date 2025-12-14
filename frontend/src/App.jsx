039ff2db8f132640f23ea03f0ae693394069c7a0
Eugene <eugyneehunter@gmail.com>
2025-12-14 23:51:34 +0300

    feat: integrate Layout component into routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Layout from './Layout/Layout'
 
 const App = () => {
   return (
     <>
       <Routes>
         {/* Define your routes here */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} /> 
+        </Route>
+
 
       </Routes>
     </>
