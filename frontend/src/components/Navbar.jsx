af6005cfd0931cd841aad540c83e555337015f70
Eugene <eugyneehunter@gmail.com>
2025-12-15 21:51:13 +0300

    Add new logo images in PNG and SVG formats
    - Introduced a new white logo in PNG format with a size of 911x168 pixels.
    - Added a corresponding SVG version of the logo, preserving the same dimensions and design.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from '../assets/images/logo-white.svg'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="w-full flex">
+            <nav className="w-full h-16 justify-between p-4 text-white">
+                <img src={logo} alt="" className='w-auto h-8' />
+            </nav>
+        </div>
     )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,19 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div className='flex flex-col items-center justify-center w-full min-h-screen'>
-            <a href="https://polymarket.com/" target="_blank" rel="noopener noreferrer">
-                <header className='w-full p-4 bg-[#2c9cdb] rounded-md mt-4 text-white text-center'>
-                    <h1 className='text-3xl font-bold'>Welcome to Polymarket Kenya</h1>
-                </header>
-            </a>
-            <main className='grow p-4'>
-                <p className='text-lg'>This is the home page of the Polymarket Kenya application.</p>
-            </main>
-            <footer className='w-full p-4 bg-gray-200 text-center'>
-                <p className='text-sm'>&copy; <span>
-                    {new Date().getFullYear()}
-                </span> Polymarket Kenya. All rights reserved.</p>
-            </footer>
+
         </div>
     )
 }
