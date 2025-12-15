c12591fca1e64e0e2d50b7caa5ae5f6d78d5f3fc
Eugene <eugyneehunter@gmail.com>
2025-12-15 21:53:55 +0300

    Implement code changes to enhance functionality and improve performance

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import logo from '../assets/images/logo-white.svg'
+import flag from '../assets/images/flag.png'
 
 const Navbar = () => {
     return (
         <div className="w-full flex">
             <nav className="w-full h-16 justify-between p-4 text-white">
-                <img src={logo} alt="" className='w-auto h-8' />
+                <div className='flex items-center gap-4'>
+                    <img src={logo} alt="" className='w-auto h-8' />
+                    <img src={flag} alt="" className='w-auto h-7 -rotate-4' />
+                </div>
+
             </nav>
         </div>
     )
