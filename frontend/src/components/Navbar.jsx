05979b7b7d6e66c5c4deab5659f8073462c97517
Eugene <eugyneehunter@gmail.com>
2025-12-15 22:08:31 +0300

    feat: enhance Navbar component layout and structure

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import logo from '../assets/images/logo-white.svg'
 import flag from '../assets/images/flag.png'
+import { MdMenu as IonsMdMenu } from 'react-icons/md'
 
 const Navbar = () => {
     return (
         <div className="w-full flex">
-            <nav className="w-full h-16 justify-between p-4 text-white">
+            <nav className="w-full flex items-center h-16 justify-between p-4 text-white">
                 <div className='flex items-center gap-4'>
                     <img src={logo} alt="" className='w-auto h-8' />
                     <img src={flag} alt="" className='w-auto h-7 -rotate-4' />
                 </div>
+
+                <input type="text"
+                    placeholder="Search polymarket /"
+                    className="bg-[#2f3f50] placeholder:text-gray-400 text-white rounded-lg px-4 py-3 w-1/3 focus:outline-none"
+                />
+
+                <a href="">How it works</a>
+
+                <div className='flex items-center gap-4'>
+                    <button className="text-[#2c9cdb] hover:text-[#35acf1] 
+                    rounded-lg hover:bg-gray-700/90 cursor-pointer items-center justify-center px-4 py-1.5 font-semibold text-lg  ">
+                        Log In
+                    </button>
+                    <button className="bg-[#2c9cdb] cursor-pointer hover:bg-[#3fb0f1] px-5 font-medium py-2 rounded-md">
+                        Sign Up
+                    </button>
+                    <IonsMdMenu size={35} className="text-white cursor-pointer hover:bg-gray-700 p-1 rounded-xl" />
+                </div>
+
 
             </nav>
         </div>
