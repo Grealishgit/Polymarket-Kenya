ea9fb116d0912323b016a785155fb256b598785f
Eugene <eugyneehunter@gmail.com>
2025-12-18 21:42:26 +0300

    feat: enhance layout of Trending page with search input and improved styling for theme support

--- frontend/src/pages/main-pages/Trending.jsx
+++ frontend/src/pages/main-pages/Trending.jsx
@@ -7,9 +7,18 @@ const Trending = () => {
 
     const { theme, setTheme: toggleTheme } = useOutletContext();
     return (
-        <div className={`flex flex-col items-center justify-center min-h-screen w-full 
+        <div className={`flex flex-col items-center pt-38 p-4  min-h-screen w-full 
         ${theme === 'dark' ? 'bg-[#1d2b3a]' : 'bg-[#ffffff]'}`}>
-            <nav>
+            <nav className='flex justify-between items-center w-full'>
+                <div>
+                    <input
+                        type="text"
+                        name="" id=""
+                        placeholder='Search ...'
+                        className={`px-4 py-2 rounded-lg w-1/3 
+                    ${theme === 'dark' ? 'bg-[#2f3f50] text-white placeholder:text-gray-400' : 'bg-[#f4f5f6] text-black placeholder:text-gray-600'}`}
+                    />
+                </div>
 
             </nav>
         </div>
