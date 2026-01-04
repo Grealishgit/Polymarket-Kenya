1934e8775da6ba1dffd0d6b66a13a585d08bb6b3
Eugene <eugyneehunter@gmail.com>
2026-01-04 18:50:11 +0300

    feat: update Footer component to use anchor tags for navigation links for improved accessibility

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,17 +4,23 @@ import React from 'react'
 const Footer = ({ themeMode, setTheme: toggleTheme }) => {
 
     return (
-        <div className={`w-full flex  
-        ${themeMode === 'dark' ? 'bg-[#1d2b3a] border-[#3d5266] text-gray-400' : 'bg-white border-gray-200'}  fixed w-full bottom-0 z-10  flex-col p-3`}>
-            <div className='w-full flex justify-between'>
+        <div className={`w-full flex font-medium
+        ${themeMode === 'dark' ? 'bg-[#1d2b3a] border-[#3d5266] text-gray-400' :
+                'bg-white border-gray-200 text-gray-400 '}  fixed w-full bottom-0 z-10  flex-col p-3`}>
+            <div className='w-full flex justify-between ml-5'>
                 <div className='flex items-center gap-1'>
                     <p className='text-sm'>Adventure One QSS Inc.ⓒ {new Date().getFullYear()}</p>
                     <ul className={`flex items-center  text-sm gap-2`}>
-                        <li>Privacy</li> •
-                        <li>Terms of use</li> •
-                        <li>Learn</li> •
-                        <li>Careers</li> •
-                        <li>Press</li>
+                        <a href='' className={`cursor-pointer 
+                            ${themeMode === 'dark' ? 'hover:text-white' : 'hover:text-black'}`}>Privacy</a> •
+                        <a href='' className={`cursor-pointer 
+                            ${themeMode === 'dark' ? 'hover:text-white' : 'hover:text-black'}`}>Terms of use</a> •
+                        <a href='' className={`cursor-pointer 
+                            ${themeMode === 'dark' ? 'hover:text-white' : 'hover:text-black'}`}>Learn</a> •
+                        <a href='' className={`cursor-pointer 
+                            ${themeMode === 'dark' ? 'hover:text-white' : 'hover:text-black'}`}>Careers</a> •
+                        <a href='' className={`cursor-pointer 
+                            ${themeMode === 'dark' ? 'hover:text-white' : 'hover:text-black'}`}>Press</a>
                     </ul>
                 </div>
                 <div>
