db601f40847e73c78bdbb2a37fd6d78e88a90cb1
Eugene <eugyneehunter@gmail.com>
2026-01-05 16:50:29 +0300

    feat: add spacing to Footer component for improved layout

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = ({ themeMode, setTheme: toggleTheme }) => {
         <div className={`w-full flex font-medium
         ${themeMode === 'dark' ? 'bg-[#1d2b3a] border-[#3d5266] text-gray-400' :
                 'bg-white border-gray-200 text-gray-400 '}  fixed w-full bottom-0 z-10  flex-col p-3`}>
+
             <div className='w-full flex justify-between ml-5'>
                 <div className='flex items-center gap-1'>
                     <p className='text-sm'>Adventure One QSS Inc.ⓒ {new Date().getFullYear()}</p>
