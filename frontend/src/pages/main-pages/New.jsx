c899eb94f660fc781391c37a0f277beace905970
Eugene <eugyneehunter@gmail.com>
2026-01-07 20:01:52 +0300

    feat: add New component with subLinks for navigation

---
+++ frontend/src/pages/main-pages/New.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const New = () => {
+
+    const subLinks = [
+        { id: 1, name: 'All', href: '' },
+        { id: 2, name: 'Trump', href: '' },
+        { id: 3, name: 'Iran', href: '' },
+        { id: 4, name: 'Greenland', href: '' },
+        { id: 5, name: 'Ukraine', href: '' },
+        { id: 6, name: 'Portugal Election', href: '' },
+        { id: 7, name: 'Minnesota Fraud', href: '' },
+        { id: 8, name: 'Epstein', href: '' },
+        { id: 9, name: 'Fed', href: '' },
+        { id: 10, name: 'Tweet Markets', href: '' },
+        { id: 11, name: 'Golden Globes', href: '' },
+        { id: 12, name: 'Silver', href: '' },
+        { id: 13, name: 'Tweet Markets', href: '' },
+        { id: 14, name: 'China', href: '' },
+        { id: 15, name: 'AI', href: '' },
+        { id: 16, name: 'Weather', href: '' },
+        { id: 17, name: 'Equalities', href: '' },
+        { id: 18, name: 'Derivatives', href: '' },
+        { id: 19, name: 'Primaries', href: '' },
+        { id: 20, name: 'Midterms', href: '' },
+        { id: 21, name: 'Movies', href: '' },
+        { id: 22, name: 'Global Elections', href: '' },
+        { id: 23, name: 'Parlays', href: '' },
+        { id: 24, name: 'Israel', href: '' },
+        { id: 25, name: 'Crypto Prices', href: '' },
+        { id: 26, name: 'Bitcoin', href: '' },
+        { id: 27, name: 'Commodities', href: '' },
+    ]
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default New
